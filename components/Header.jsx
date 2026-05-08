de1dd1471188326e4d01d14a994986912eec5144
Kaneloth <kanelothelejane@gmail.com>
2026-05-08 19:31:14 +0200

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
  return(
   <div className="p-4 bg-white shadow flex gap-3 items-center">
-   <div className="bg-blue-600 text-white p-2 rounded">🚴</div>
+   <div className="bg-blue-600 text-white px-4 py-2 rounded-2xl shadow-md hover:shadow-xl active:scale-95 transition">🚴</div>
    <h1>Courio</h1>
   </div>
  )
