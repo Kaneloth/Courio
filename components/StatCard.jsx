105dc0279221d5718c9be0a17e429f3b50896f6c
Kaneloth <kanelothelejane@gmail.com>
2026-05-08 19:38:33 +0200

    Update StatCard.jsx

--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -1,7 +1,11 @@
-export function StatCard({title,value}){
- return(
-  <div className="bg-white p-4 shadow rounded">
-   <p>{title}</p><h2>{value}</h2>
-  </div>
- )
+export function StatCard({ title, value }) {
+  return (
+    <div className="bg-white p-4 rounded-2xl shadow-md hover:shadow-xl transition">
+      <p className="text-sm text-gray-500">{title}</p>
+
+      <h2 className="text-2xl font-bold mt-1">
+        {value}
+      </h2>
+    </div>
+  );
 }
