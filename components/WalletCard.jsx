21c91c3ff6b06388e85a776ef7078cd1d0722018
Kaneloth <kanelothelejane@gmail.com>
2026-05-08 19:33:47 +0200

    Update WalletCard.jsx

--- components/WalletCard.jsx
+++ components/WalletCard.jsx
@@ -1,6 +1,6 @@
 export function WalletCard(){
  return(
-  <div className="bg-blue-600 text-white p-4 rounded">
+  <div className="bg-blue-600 text-white px-4 py-2 rounded-2xl shadow-md hover:shadow-xl active:scale-95 transition">
    <p>Wallet Balance</p>
    <h2>R 0,00</h2>
   </div>
